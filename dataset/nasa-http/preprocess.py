e28ae6b47aadd12c1ab6bd4c06243d3574dd72e7
ehsangolshani <ehsanroman74@gmail.com>
2019-09-01 08:15:13 +0430

    fixed index problem of target csv in nasa dataset preprocess, refactor code

--- dataset/nasa-http/preprocess.py
+++ dataset/nasa-http/preprocess.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-if __name__ == '__main__':
 
-    # in seconds
-    time_step = 30
-    df = pd.read_csv('NASA_access_log_Aug95.csv', delimiter=',')
+def preprocess(source_csv_path: str, output_csv_path: str, time_step: int = 60):
+    df = pd.read_csv(source_csv_path, delimiter=',')
     df = df[['day', 'hour', 'minute', 'seconds_from_start']]
     req_num_df = pd.DataFrame(columns=['day', 'hour', 'minute', 'request_numbers'])
     print(df)
@@ -14,22 +12,25 @@ if __name__ == '__main__':
     start_step = df.loc[0]['seconds_from_start']
     next_step = start_step + time_step
 
+    new_df_index = 0
     for i, row in df.iterrows():
         if row['seconds_from_start'] < next_step:
             req_num += 1
         else:
-            req_num_df = req_num_df.append(
-                {'day': row['day'],
-                 'hour': row['hour'],
-                 'minute': row['minute'],
-                 'request_numbers': req_num
-                 }, ignore_index=True)
+            req_num_df.loc[new_df_index] = [row['day'], row['hour'], row['minute'], req_num]
             req_num = 0
+            new_df_index += 1
             start_step = next_step
             next_step = start_step + time_step
 
-        if i > 10000:
-            break
+            if new_df_index % 10000 == 0:
+                print(new_df_index)
 
     print(req_num_df)
-    req_num_df.to_csv("nasa_temporal_request_number_dataset.csv", sep=',')
+    req_num_df.to_csv(output_csv_path, sep=',', index=False)
+
+
+if __name__ == '__main__':
+    # in seconds
+    preprocess('NASA_access_log_Aug95.csv', 'nasa_temporal_request_number_dataset_August95.csv', 30)
+    preprocess('NASA_access_log_Jul95.csv', 'nasa_temporal_request_number_dataset_July95.csv', 30)
