2de6c9728067f5c270c689cf8345e672e58f4163
ehsangolshani <ehsanroman74@gmail.com>
2019-12-23 12:44:51 +0330

    change number of layers to 1 in one-to-one implementation of LSTM

--- one_to_one_run_with_lstm.py
+++ one_to_one_run_with_lstm.py
@@ -11,7 +11,7 @@ hidden_dim = 1
 input_size = 1
 output_size = 1
 batch_size = 1
-num_layers = 3
+num_layers = 1
 
 workload_dataset_july = SimpleWorkloadDataset(
     csv_path='raw_dataset/nasa_http/nasa_temporal_rps_July95_1m.csv'
